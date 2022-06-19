32f444b701ed316a9779f5fa44613e937f5f2af5
RuzaikinDY <Devilwind@mail.ru>
2022-06-19 22:04:20 +0300

    Reformate code

--- src/test/java/ru/netology/manager/FilmManagerTest.java
+++ src/test/java/ru/netology/manager/FilmManagerTest.java
@@ -5,33 +5,34 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.FilmProperties;
 
 public class FilmManagerTest {
-    FilmManager manager=new FilmManager();
-    FilmProperties first = new FilmProperties(1,"First Blood","action");
-    FilmProperties second = new FilmProperties(2,"Spider man","action");
-    FilmProperties third = new FilmProperties(3,"Transformers","action");
-    FilmProperties fourth = new FilmProperties(4,"Rambo","action");
-    FilmProperties fifth = new FilmProperties(5,"Predator","action");
-    FilmProperties sixth = new FilmProperties(6,"Rampage","action");
-    FilmProperties seventh = new FilmProperties(7,"Transformers 2","action");
-    FilmProperties eights = new FilmProperties(8,"Jurassic Park","action");
-    FilmProperties ninths = new FilmProperties(9,"Independence Day","action");
-    FilmProperties tenth = new FilmProperties(10,"Alien","action");
-    FilmProperties eleventh = new FilmProperties(11,"Star Wars","action");
-
-
-@Test
-    public void shouldAddItems(){
     FilmManager manager = new FilmManager();
-    manager.addFilm(first);
-    manager.addFilm(second);
-    manager.addFilm(third);
-    manager.addFilm(fourth);
-    manager.addFilm(fifth);
-    manager.addFilm(sixth);
-    FilmProperties[] actual = manager.findAll();
-    FilmProperties[] expected = {first,second,third,fourth,fifth,sixth};
-    Assertions.assertArrayEquals(expected,actual);
+    FilmProperties first = new FilmProperties(1, "First Blood", "action");
+    FilmProperties second = new FilmProperties(2, "Spider man", "action");
+    FilmProperties third = new FilmProperties(3, "Transformers", "action");
+    FilmProperties fourth = new FilmProperties(4, "Rambo", "action");
+    FilmProperties fifth = new FilmProperties(5, "Predator", "action");
+    FilmProperties sixth = new FilmProperties(6, "Rampage", "action");
+    FilmProperties seventh = new FilmProperties(7, "Transformers 2", "action");
+    FilmProperties eights = new FilmProperties(8, "Jurassic Park", "action");
+    FilmProperties ninths = new FilmProperties(9, "Independence Day", "action");
+    FilmProperties tenth = new FilmProperties(10, "Alien", "action");
+    FilmProperties eleventh = new FilmProperties(11, "Star Wars", "action");
+
+
+    @Test
+    public void shouldAddItems() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm(first);
+        manager.addFilm(second);
+        manager.addFilm(third);
+        manager.addFilm(fourth);
+        manager.addFilm(fifth);
+        manager.addFilm(sixth);
+        FilmProperties[] actual = manager.findAll();
+        FilmProperties[] expected = {first, second, third, fourth, fifth, sixth};
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldCheckLimitFiveFilms() {
         FilmManager manager = new FilmManager(5);
@@ -42,9 +43,10 @@ public class FilmManagerTest {
         manager.addFilm(eleventh);
 
         FilmProperties[] actual = manager.findLastAdd();
-        FilmProperties[] expected = {eleventh,ninths,sixth,fifth,first};
-        Assertions.assertArrayEquals(expected,actual);
+        FilmProperties[] expected = {eleventh, ninths, sixth, fifth, first};
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldCheckItemsOverLimitOfFourItems() {
         FilmManager manager = new FilmManager(3);
@@ -54,9 +56,10 @@ public class FilmManagerTest {
         manager.addFilm(fourth);
 
         FilmProperties[] actual = manager.findLastAdd();
-        FilmProperties[] expected = {fourth,third,second};
-        Assertions.assertArrayEquals(expected,actual);
+        FilmProperties[] expected = {fourth, third, second};
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldCheckItemOverLimitWithoutMeaningfilmtoShow() {
         FilmManager manager = new FilmManager();
@@ -73,8 +76,8 @@ public class FilmManagerTest {
         manager.addFilm(eleventh);
 
         FilmProperties[] actual = manager.findLastAdd();
-        FilmProperties[] expected = {eleventh,tenth,ninths,eights,seventh,sixth,fifth,fourth,third,second};
-        Assertions.assertArrayEquals(expected,actual);
+        FilmProperties[] expected = {eleventh, tenth, ninths, eights, seventh, sixth, fifth, fourth, third, second};
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 
